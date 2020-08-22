dd6e96458c9d158b32ef2677f673e04732be92a3
bishalraktim <raktimbishal@gmail.com>
2020-08-22 21:26:09 +1000

    Updates

---
+++ precache-manifest.958de56da8bf8ba4d4bf5b075f95ae2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c43cd89b68ceeaabc69e1a4858f8e4c",
+    "url": "/Random-Quote-Machine/index.html"
+  },
+  {
+    "revision": "8efb3c84db4ac63bc9ba",
+    "url": "/Random-Quote-Machine/static/css/main.2e7bc66f.chunk.css"
+  },
+  {
+    "revision": "7e8d081f139adb55ccf0",
+    "url": "/Random-Quote-Machine/static/js/2.2e4e973f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Random-Quote-Machine/static/js/2.2e4e973f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8efb3c84db4ac63bc9ba",
+    "url": "/Random-Quote-Machine/static/js/main.0dc669f1.chunk.js"
+  },
+  {
+    "revision": "11bdb18a23af74bccada",
+    "url": "/Random-Quote-Machine/static/js/runtime-main.bf8f766a.js"
+  }
+]);
